3687b5fe7bbcb574debb02dbbc9b16f6008f541b
Temitope Ekoh <103740128+topsyLord3@users.noreply.github.com>
2023-02-26 21:26:37 -1200

    updated js file
    Todo task completed

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const num = document.querySelector('span');
 const container = document.querySelector('.box-container');
 const invalid = document.querySelector('.invalid');
 
+button.disabled = true;
+button.style.opacity = '0.6'
+
 let ratePoint;
 
 rates.addEventListener('click', point);
@@ -14,6 +17,8 @@ rates.addEventListener('click', point);
 function point(e) {
         if (e.target.className === 'rate-score') {
                 ratePoint = e.target.innerText;
+                button.disabled = false;
+                button.style.opacity = '1';
         }
 }
 
@@ -22,11 +27,9 @@ button.addEventListener('click', onClick);
 //TODO: disable button when user has not provided an option
 //TODO: only enable when user has provided an option
 function onClick(e) {
-        if (button.classList.contains('btn')) {
                 num.innerText = ratePoint;
                 ratingFeed.style.display = 'block';
                 ratingFeed.style.zIndex = '1';
-        }
         if (ratePoint === undefined) {
                 questionRating.style.display = 'block';
                 ratingFeed.style.zIndex = '-1';
